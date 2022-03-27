9db513c85ba32e02341dbed13b980c386c61e396
YieldRay <565332016@qq.com>
2022-03-27 21:59:18 +0800

    hide node.js

--- test/develop.mjs
+++ test/develop.mjs
@@ -1,6 +1,6 @@
 // 本地测试
-import http from 'http';
-import handler from './node.mjs';
+import http from "http";
+import handler from "./node.mjs";
 const port = process.env.PORT || 8000;
 console.log(`Listening on ${port}`);
 http.createServer(handler).listen(port);
